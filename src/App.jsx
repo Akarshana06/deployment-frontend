7b24e070e04adb4884b9dc373a26795594a37dde
Akarshana06 <akarshana027@gmail.com>
2025-02-26 10:38:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return(
+    <div className="intro">
+      <header>
+        <h1>Akarshana</h1>
+        <p className="hobby">Love to learn books which are crime thriller and motivational books </p>
+      </header>
+
+      <main>
+        <section className="details">
+          <ul>
+            <li>Email: your.email@afsds.com</li>
+            <li>Phone: 91+ 9897507609</li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
